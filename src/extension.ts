aae187e32a4522e9d0e2e4233578a53c379491ff
Hilmar Gústafsson <hilmargustafs@gmail.com>
2017-06-09 07:20:01 +0200

    TCC: Compile works

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 "use strict";
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as path from "path";
 
@@ -13,46 +12,78 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log("TCC-Compiler is active.");
+  const execSync = require("child_process").execSync;
+  const execFile = require("child_process").execFile;
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
+
+  /**
+	 * TCC: Run
+	 */
   context.subscriptions.push(
     vscode.commands.registerCommand("tcc-compiler.run", () => {
       checkTerminal();
-      getLatestTerminal().sendText(
-        path
-          .join(context.extensionPath, "/Compiler/tcc.exe")
-          .concat(' -run "' + getFileName() + '"')
-      );
+      getLatestTerminal().sendText(tcc(' -run "' + getFileName() + '"'));
       getLatestTerminal().show();
     })
   );
 
+  /**
+	 * TCC: Run with flags...
+	 */
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "tcc-compiler.runWithFlags",
       (flags: string) => {
-        this;
         checkTerminal();
         getLatestTerminal().sendText(
-          path
-            .join(context.extensionPath, "/Compiler/tcc.exe")
-            .concat(flags + ' -run "' + getFileName() + '"')
+          tcc(flags + ' -run "' + getFileName() + '"')
         );
         getLatestTerminal().show();
       }
     )
   );
+
+  /**
+	 * TCC: Compile
+	 */
   context.subscriptions.push(
     vscode.commands.registerCommand("tcc-compiler.compile", () => {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World!");
+      checkTerminal();
+      var loc = path.join(__dirname, "..", "..");
+      var fname = vscode.window.activeTextEditor.document.fileName;
+      var outname = fname.split("\\");
+      var cnt = outname.length;
+      var outname = outname[cnt - 1].split(".");
+      var output = path.join(
+        vscode.window.activeTextEditor.document.fileName,
+        "..",
+        outname[0] + ".exe"
+      );
+      try {
+        let cmd = tcc(' "' + getFileName() + '" -o "' + output + '"');
+        execSync(cmd);
+        vscode.window.showInformationMessage("Compile success!");
+      } catch (error) {
+        // TODO: Do this better.
+        const editor = vscode.window.activeTextEditor;
+        var errormsg = error.message.split("/");
+        vscode.window.showErrorMessage(errormsg[errormsg.length - 1]);
+        var line = errormsg[errormsg.length - 1].split(":");
+        var position = editor.selection.active;
+        var newPosition = position.with(parseInt(line[1]) - 1, 100);
+        var StartPosition = position.with(parseInt(line[1]) - 1, 0);
+        var newSelection = new vscode.Selection(StartPosition, newPosition);
+        editor.selection = newSelection;
+      }
     })
   );
 
+  /**
+	 * Creates a new terminal if none exist.
+	 */
   function checkTerminal() {
     if (0 === terminalStack.length) {
       let terminal = vscode.window.createTerminal(
@@ -62,11 +93,25 @@ export function activate(context: vscode.ExtensionContext) {
     }
   }
 
+  /**
+	 * Gets the last terminal.
+	 */
   function getLatestTerminal() {
     return terminalStack[terminalStack.length - 1];
   }
 
+  /**
+	 * Gets the name of the current C file.
+	 */
   function getFileName() {
     return vscode.window.activeTextEditor.document.fileName.toString();
+  }
+
+  /**
+	 * Gets the path of TCC.
+	 * @param args Arguments for Tiny C Compiler.
+	 */
+  function tcc(args: string) {
+    return path.join(context.extensionPath, "/Compiler/tcc.exe").concat(args);
   }
 }
